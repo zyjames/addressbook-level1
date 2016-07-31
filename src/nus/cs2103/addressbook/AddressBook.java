2b498070e7330aee26a0e41a15c8952fb7a82238
Damith C. Rajapakse <damith@gmail.com>
2016-07-31 15:08:35 +0800

    minor tweaks to processProgramArgs

--- src/nus/cs2103/addressbook/AddressBook.java
+++ src/nus/cs2103/addressbook/AddressBook.java
@@ -225,17 +225,17 @@ public class AddressBook {
      * @param args full program arguments passed to application main method
      */
     private static void processProgramArgs(String[] args) {
-        if (args.length > 1) {
+        if (args.length >= 2) {
             showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
             exitProgram();
         }
 
-        if(args.length == 0) {
-            setupDefaultFileForStorage();
-        }
-
         if (args.length == 1) {
             setupGivenFileForStorage(args[0]);
+        }
+
+        if(args.length == 0) {
+            setupDefaultFileForStorage();
         }
 
     }
