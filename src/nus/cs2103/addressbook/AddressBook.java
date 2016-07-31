910220a434f01baa38fb7b19dbf61b0efa455752
Leow Yijin <leowyijin@gmail.com>
2016-07-31 22:29:40 +0800

    mention case-sensitivity in find command header comment

--- src/nus/cs2103/addressbook/AddressBook.java
+++ src/nus/cs2103/addressbook/AddressBook.java
@@ -387,6 +387,7 @@ public class AddressBook {
 
     /**
      * Finds and lists all persons in address book whose name contains any of the argument keywords.
+     * Keyword matching is case sensitive.
      * 
      * @param commandArgs full command args string from the user
      * @return feedback display message for the operation result
