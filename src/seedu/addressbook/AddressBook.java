9c8705274eecf95f6f1c0c9626cd2c09aa8145d1
Zhiyuan <jameslaizy@gmail.com>
2017-01-26 03:36:22 +0800

    Change find command comment to not case sensitive

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -421,7 +421,7 @@ public class AddressBook {
 
     /**
      * Finds and lists all persons in address book whose name contains any of the argument keywords.
-     * Keyword matching is case sensitive.
+     * Keyword matching is not case sensitive.
      *
      * @param commandArgs full command args string from the user
      * @return feedback display message for the operation result
